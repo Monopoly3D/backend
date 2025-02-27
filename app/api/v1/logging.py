56a0f8efbb77e2012dbd83b85fc204a78f5e2b31
Plummy <plummybeatsoff@gmail.com>
2025-02-27 20:42:01 +0100

    Added file reading instead of log dictionary

--- app/api/v1/logging.py
+++ app/api/v1/logging.py
@@ -1,40 +1,8 @@
+import json
 import logging
 from typing import Any, Dict
 
-logger_name: str = "monopoly"
+with open("app/api/v1/logging.json", "r", encoding="utf-8") as f:
+    API_LOG_CONFIG: Dict[str, Any] = json.load(f)
 
-API_LOG_CONFIG: Dict[str, Any] = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "[%(asctime)s] %(levelprefix)s %(message)s",
-            "use_colors": None
-        },
-        "access": {
-            "()": "uvicorn.logging.AccessFormatter",
-            "fmt": '[%(asctime)s] %(levelprefix)s %(client_addr)s "%(request_line)s" %(status_code)s'
-        }
-    },
-    "handlers": {
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr"
-        },
-        "access": {
-            "formatter": "access",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout"
-        }
-    },
-    "loggers": {
-        logger_name: {"handlers": ["default"], "level": "INFO", "propagate": False},
-        "uvicorn": {"handlers": ["default"], "level": "INFO", "propagate": False},
-        "uvicorn.error": {"level": "INFO"},
-        "uvicorn.access": {"handlers": ["access"], "level": "INFO", "propagate": False}
-    }
-}
-
-logger = logging.getLogger(logger_name)
+logger = logging.getLogger("monopoly")
