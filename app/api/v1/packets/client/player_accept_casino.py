d857ea215665b1e65a68992720b82d7de12c56b4
Plummy <plummybeatsoff@gmail.com>
2025-03-08 14:26:06 +0100

    Made all client packets dataclasses

--- app/api/v1/packets/client/player_accept_casino.py
+++ app/api/v1/packets/client/player_accept_casino.py
@@ -1,26 +1,18 @@
 from typing import Dict, Any, List
 from uuid import UUID
 
-from app.api.v1.exceptions.http.invalid_packet import InvalidPacketError
+from pydantic.dataclasses import dataclass
+
 from app.api.v1.packets.base_client import ClientPacket
 
 
+@dataclass
 class ClientPlayerAcceptCasinoPacket(ClientPacket):
     PACKET_TAG = "player_accept_casino"
 
-    PACKET_KEYS = ["game_id", "dices"]
-
-    def __init__(
-            self,
-            game_id: UUID,
-            dices: List[int]
-    ) -> None:
-        self.game_id = game_id
-        self.dices = dices
+    game_id: UUID
+    dices: List[int]
 
     @classmethod
     def from_json(cls, packet: Dict[str, Any]) -> 'ClientPacket':
-        try:
-            return cls(UUID(packet["game_id"]), packet["dices"])
-        except ValueError:
-            raise InvalidPacketError("Provided packet data is invalid")
+        return cls(UUID(packet["game_id"]), packet["dices"])
--- app/api/v1/packets/client/player_ready.py
+++ app/api/v1/packets/client/player_ready.py
@@ -1,29 +1,18 @@
 from typing import Dict, Any
 from uuid import UUID
 
-from app.api.v1.exceptions.http.invalid_packet import InvalidPacketError
+from pydantic.dataclasses import dataclass
+
 from app.api.v1.packets.base_client import ClientPacket
 
 
+@dataclass
 class ClientPlayerReadyPacket(ClientPacket):
     PACKET_TAG = "player_ready"
 
-    PACKET_KEYS = ["game_id", "is_ready"]
-
-    def __init__(
-            self,
-            game_id: UUID,
-            is_ready: bool
-    ) -> None:
-        self.game_id = game_id
-        self.is_ready = is_ready
+    game_id: UUID
+    is_ready: bool
 
     @classmethod
     def from_json(cls, packet: Dict[str, Any]) -> 'ClientPacket':
-        try:
-            return cls(
-                UUID(packet["game_id"]),
-                packet["is_ready"]
-            )
-        except ValueError:
-            raise InvalidPacketError("Provided packet data is invalid")
+        return cls(UUID(packet["game_id"]), packet["is_ready"])
