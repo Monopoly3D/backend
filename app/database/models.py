012a0c9a4e8afb009464e32813fbfa156ffc2cc4
Plummy <plummybeatsoff@gmail.com>
2025-08-17 14:43:02 +0200

    Separate refresh tokens

--- app/database/models.py
+++ app/database/models.py
@@ -16,14 +16,22 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(True), primary_key=True, server_default=func.gen_random_uuid())
-    username = Column(String(), unique=True, nullable=False)
+    username = Column(String(20), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     password_hash = Column(String(), nullable=False)
-    refresh_token = Column(String(), nullable=True, default=None)
     created_at = Column(DateTime(), default=datetime.now, nullable=False)
     updated_at = Column(DateTime(), nullable=True, onupdate=datetime.now)
 
     roles = relationship("UserRole", back_populates="user")
+
+
+class UserRefreshToken(Base):
+    __tablename__ = "user_refresh_tokens"
+
+    user_id = Column(UUID(True), ForeignKey("users.id"), primary_key=True, nullable=False)
+    refresh_token = Column(String(), nullable=False)
+    created_at = Column(DateTime(), default=datetime.now, nullable=False)
+    updated_at = Column(DateTime(), nullable=True, onupdate=datetime.now)
 
 
 class UserRole(Base):
