dcacb29f112c83055fe2f5829893d3db25612bcf
Plummy <plummybeatsoff@gmail.com>
2025-08-18 17:48:27 +0200

    Remove secureness until we get https

--- app/api/v1/security/authenticator.py
+++ app/api/v1/security/authenticator.py
@@ -127,8 +127,8 @@ class Authenticator:
             "refresh_token",
             refresh_token,
             httponly=True,
-            secure=True,
-            samesite="none"
+            secure=False,
+            samesite="lax"
         )
 
     async def create_register_token(
