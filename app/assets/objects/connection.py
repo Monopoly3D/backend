aec139c27277636c1473958507df6a5cd7ae2729
Plummy <plummybeatsoff@gmail.com>
2025-08-24 21:23:18 +0200

    Add auto removal for connection object

--- app/assets/objects/connection.py
+++ app/assets/objects/connection.py
@@ -1,18 +1,26 @@
+import asyncio
 from asyncio import CancelledError
-from typing import Any
+from typing import Any, TYPE_CHECKING, Annotated
 
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
 from app.api.v1.packets.base_server import ServerPacket
 
+if TYPE_CHECKING:
+    from app.assets.objects.connections import Connections
+
 
 class Connection(WebSocket):
+    __CONNECTION_REMOVAL_DELAY: float = 0.5
+
     def __init__(
             self,
             *args: Any,
+            connections: Connections,
             **kwargs: Any
     ) -> None:
         super().__init__(*args, **kwargs)
+        self.connections = connections
 
     async def send_packet(
             self,
@@ -21,8 +29,17 @@ class Connection(WebSocket):
         try:
             await self.send_text(packet.pack())
         except (WebSocketDisconnect, RuntimeError, CancelledError):
-            pass
+            await asyncio.sleep(self.__CONNECTION_REMOVAL_DELAY)
+            await self.connections.remove_connection(
+                await self.connections.get_user_id(self)
+            )
 
     @staticmethod
-    def dependency(websocket: WebSocket) -> 'Connection':
-        return Connection(websocket)
+    def dependency(
+            websocket: WebSocket,
+            connections: Annotated[Connections, Connections.dependency]
+    ) -> 'Connection':
+        return Connection(
+            websocket,
+            connections=connections
+        )
